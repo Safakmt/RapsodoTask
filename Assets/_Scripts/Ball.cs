59841e0627d8d4aeae505267435b7377799a93ef
Safakmt <77243118+Safakmt@users.noreply.github.com>
2024-11-24 21:31:08 +0300

    Update Ball prefab

---
+++ Assets/_Scripts/Ball.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public TextMeshProUGUI pointUi;
+    public int point;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
