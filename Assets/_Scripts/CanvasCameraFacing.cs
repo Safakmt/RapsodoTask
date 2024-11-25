472b394d3b55e1a630e07de1cdd512aa3924c4b6
Safakmt <77243118+Safakmt@users.noreply.github.com>
2024-11-25 15:29:03 +0300

    Add CanvasCameraFacing.cs

---
+++ Assets/_Scripts/CanvasCameraFacing.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasCameraFacing : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    void Start()
+    {
+
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (mainCamera != null)
+        {
+
+            transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
+                             mainCamera.transform.rotation * Vector3.up);
+        }
+    }
+}
